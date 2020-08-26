3b680df9833262fa5a66bbb8be20bc6e78ebcd63
kultosh <kulgole90@gmail.com>
2020-08-26 20:51:25 +0545

    Learned routing html pages

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.send('<p>Home Page</p>');
+    // res.send('<p>Home Page</p>');
+    res.sendFile('./views/index.html', {root: __dirname});
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', {root: __dirname});
 });
