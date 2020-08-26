6f2ee969cb92304e9aa32e78826c40012f24a4d0
kultosh <kulgole90@gmail.com>
2020-08-26 21:31:09 +0545

    Install ejs for the template by command npm install ejs

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+// express app
 const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    // res.send('<p>Home Page</p>');
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.sendFile('./views/index.html', {
+        root: __dirname
+    });
 });
 
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.sendFile('./views/about.html', {
+        root: __dirname
+    });
 });
 
 
@@ -22,5 +29,7 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).sendFile('./views/404.html', {
+        root: __dirname
+    });
 });
