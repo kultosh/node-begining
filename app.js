cf58afafefb048086dad7a1e243c97d0beff255d
kultosh <kulgole90@gmail.com>
2020-08-26 22:31:53 +0545

    Passing data into views in short created server side rendering

--- app.js
+++ app.js
@@ -9,20 +9,25 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.render('index');
+    const blogs = [
+        {title: 'Yoshi find eggs', snippet: 'Occaecat excepteur qui non incididunt proident cupidatat commodo.'},
+        {title: 'Mario find stars', snippet: 'Occaecat excepteur qui non incididunt proident cupidatat commodo.'},
+        {title: 'How to defeat bower', snippet: 'Occaecat excepteur qui non incididunt proident cupidatat commodo.'},
+    ]
+    res.render('index', {title: 'Home', blogs});
 });
 
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', {title: 'About'});
 });
 
 app.get('/blogs/create', (req, res) => {
-    res.render('create');
+    res.render('create', {title: 'Create Blog'});
 });
 
 
 // 404 page
 app.use((req, res) => {
-    res.status(404).render('404');
+    res.status(404).render('404', {title: '404 Error!'});
 });
