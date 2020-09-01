b269faefa16d243ff7909e7f466b553e8b9ccce2
kultosh <kulgole90@gmail.com>
2020-09-01 09:36:01 +0545

    Learned to add route name as variable in app

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,7 +3,7 @@ const Blog = require('../models/blog');
 
 const router = express.Router();
 
-router.get('/blogs', (req, res) => {
+router.get('/', (req, res) => {
     Blog.find().sort({
             createdAt: -1
         })
@@ -15,13 +15,13 @@ router.get('/blogs', (req, res) => {
         })
 });
 
-router.get('/blogs/create', (req, res) => {
+router.get('/create', (req, res) => {
     res.render('create', {
         title: 'Create Blog'
     });
 });
 
-router.post('/blogs', (req, res) => {
+router.post('/', (req, res) => {
     const blog = new Blog(req.body);
 
     blog.save()
@@ -33,7 +33,7 @@ router.post('/blogs', (req, res) => {
         })
 });
 
-router.get('/blogs/:id', (req, res) => {
+router.get('/:id', (req, res) => {
     const id = req.params.id;
     Blog.findById(id)
         .then((result) => {
@@ -47,7 +47,7 @@ router.get('/blogs/:id', (req, res) => {
         })
 });
 
-router.delete('/blogs/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
     const id = req.params.id;
 
     Blog.findByIdAndDelete(id)
